194836b00c7ffb4f4b2c3603104a1c6fbd57cfdf
Valerie Woolard <vwoolard@salesforce.com>
2021-11-30 12:39:06 -0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ puts "Creating fans"
+ Fan.create(name: "Joey", enthusiasm_level: 10, favorite_player: "Gritty", email: "joey@joey.com", hooliganism_level: 10)
+ Fan.create(name: "Katie", enthusiasm_level: 10, favorite_player: "Lo", email: "katie@example.com", hooliganism_level: 1)
+ Fan.create(name: "Gina", enthusiasm_level: 1, favorite_player: "Burt", email: "gina@example.com", hooliganism_level: 5)
+ Fan.create(name: "John", enthusiasm_level: 5, favorite_player: "Burt", email: "john@example.com", hooliganism_level: 5)
+ Fan.create(name: "🐪🐪🐪🐪", enthusiasm_level: 5, favorite_player: "Burt", email: "camel@example.com", hooliganism_level: 5)
