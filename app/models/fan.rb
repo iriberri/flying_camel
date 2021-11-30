4b170fe4d44a4aff5d4f1fb18d583e25e9d010f7
Kenny Parnell <k.parnell@gmail.com>
2021-11-30 15:38:26 -0500

    Validate enthusiasm.

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,2 +1,7 @@
 class Fan < ApplicationRecord
+  validates :enthusiasm_level, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+
+  def fan_pic
+    "fan-#{self.enthusiasm_level}.png"
+  end
 end
